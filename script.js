cff07bf3ae1a08b93649a1afbb9bb39b9e29c869
Flavio fernando da Silva <tchfernando@gmail.com>
2021-07-09 13:39:52 -0300

    add play again function

--- script.js
+++ script.js
@@ -5,8 +5,9 @@
   console.log(123);
 }*/
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highestScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -20,11 +21,15 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Número Correto!';
     document.querySelector('.number').textContent = secretNumber;
-
     
     document.querySelector('body').style.backgroundColor = '#60b347';
 
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highestScore) {
+      highestScore = score;
+      document.querySelector('.highestScore').textContent = highestScore
+    }
 
     // When guess is to high
   } else if (guess > secretNumber) {
@@ -49,7 +54,19 @@ document.querySelector('.check').addEventListener('click', function () {
     } else {
       document.querySelector('.message').textContent =
       '💥 Você Perdeu!';
-        document.querySelector('.score').textContent = 0;
+      document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function() {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector('.message').textContent = 'Adivinhando...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').textContent = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
